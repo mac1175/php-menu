458fd27ec53a3e7b1154a6badc3c7d4e121579db
mac1175 <mac1175@gmail.com>
2013-01-21 22:35:36 -0500

    qwdwdqwdqwd
     Please enter the commit message for your changes. Lines starting

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
-<?php 
-
+<?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>test</title>
+        <style type="text/css">
+
+        </style>
     </head>
     <body>
         <?php
@@ -17,55 +18,66 @@ ini_set('display_errors', '1');
 
             var $page;
             var $name;
+            var $subMenus=array(); // array of SubMenus
 
             function Menu($page_, $name_) {
                 $this->page = $page_;
                 $this->name = $name_;
             }
 
+            function &addSubMenu($page, $name) {
+                $menu = new Menu($page, $name);
+                array_push($this->subMenus, $menu);
+                return $menu;
+            }
+
+            function getSubMenus() {
+                return $this->subMenus;
+            }
+
         }
 
         class TopMenu extends Menu {
- 
-            var $subMenus; // array of SubMenus
 
             function TopMenu($page_, $name_) {
                 $this->page = $page_;
                 $this->name = $name_;
                 $this->subMenus = array();
             }
-
-            function addSubMenu($menu) {
-                array_push($this->subMenus, $menu);
-            }
-            function getSubMenus(){
-                return $this->subMenus;
-            }
-
         }
 
         class MenuManager {
 
-            var $menu_items;
+            var $menu_items = array();
 
-            function MenuManager($items) {
-                $this->menu_items = $items;
-                 
+            public function &AddTopMenu($key, $page, $name) {
+                $menu = new TopMenu($page, $name);
+                $this->menu_items[$key] = $menu;
+                return $menu;
+            }
+
+            public function &addSubMenu($key, $page, $name) {
+                if (array_key_exists($key, $this->menu_items)) {
+                    return $this->menu_items[$key]->addSubMenu($page, $name);
+                }
+                echo "$key not found";
+                return NULL;
             }
 
             function displayLandingPageBulletList() {
-                echo '<ul id="memberMenu">';
-                for ($i = 0; $i < count($this->menu_items); $i++) {
-                    $menu = $this->menu_items[$i];
+                echo '<ul class="top-menu-item">';
+                $keys = array_keys($this->menu_items);
+                foreach ($this->menu_items as $key => $value) {
+                    $menu = $this->menu_items[$key];
                     $this->createMenuItem($menu);
                 }
                 echo '</ul>';
             }
 
-            function createMenuItem($menu) {
-                
+            private function createMenuItem($menu) {
+
                 echo "<li><a href='$menu->page' >$menu->name</a>";
-                if (is_subclass_of($menu, 'Menu') && count($menu->getSubMenus()) > 0) {
+                if (count($menu->getSubMenus()) > 0) {
                     echo '<ul>';
                     $subs = $menu->getSubMenus();
                     for ($m = 0; $m < count($subs); $m++
@@ -80,19 +92,19 @@ ini_set('display_errors', '1');
 
         }
 
-        $tm_array = array();
-        for ($tc = 0; $tc < 10; $tc++) {
-            $n="tm$tc";
-            $tm = new TopMenu($n, $n);
-            for ($x = 0; $x < 5; $x++) {
-                $sn=$n."_sub$x";
-                $tm->addSubMenu(new Menu($sn, $sn));
-            }
-            array_push($tm_array, $tm);
-        }
-        
-        $mm = new MenuManager($tm_array);
+//usage
+        $mm = new MenuManager();
+        $mm->AddTopMenu("topmenu1", "/some/page", "top menu 1");
         //var_dump($mm);
+        $mm->addSubMenu("topmenu1", "/some/page", "sub menu 1");
+
+        $mm->addTopMenu("topmenu2", "/some/page", "top menu 2");
+
+        $x= $mm->addSubMenu("topmenu1", "/some/page", "sub menu 2");
+var_dump($x);
+$x->addSubMenu("asdasd","wergwrgw");
+
+//var_dump($mm);
         $mm->displayLandingPageBulletList();
         ?>
     </body>
